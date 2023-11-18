6e741844ff44824e3b489989c92ea9d95afc43f2
10decoders <10decoders@DESKTOP-RD0I1G3>
2023-11-18 23:43:13 +0530

    Add project files.

---
+++ Task2-CRUDApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task2_CRUDApp.Data;
+using Task2_CRUDApp.Models;
+
+namespace Task2_CRUDApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Task2_CRUDAppContext _context;
+
+        public HomeController(Task2_CRUDAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return _context.Product != null ?
+                          View(await _context.Product.ToListAsync()) :
+                          Problem("Entity set 'Task2_CRUDAppContext.Product'  is null.");
+            
+        }
+
+       
+    }
+}
---
+++ Task2-CRUDApp/Data/Task2_CRUDAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Task2_CRUDApp.Models;
+
+namespace Task2_CRUDApp.Data
+{
+    public class Task2_CRUDAppContext : DbContext
+    {
+        public Task2_CRUDAppContext (DbContextOptions<Task2_CRUDAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Task2_CRUDApp.Models.Product> Product { get; set; } = default!;
+    }
+}
